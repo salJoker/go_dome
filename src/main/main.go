95c602541277874d1504c929fb8153144cd3c071
salJoker <huangchao387@sina.com>
2015-08-08 02:28:46 +0800

    use echo

--- src/main/main.go
+++ src/main/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo"
+	mw "github.com/labstack/echo/middleware"
+)
 
 func main(){
-	fmt.Println("the is main for master branch")
+	echo_serve := echo.New()
+
+	mws := []echo.Middleware{mw.Logger(),mw.Recover(),mw.Gzip()}
+	//注册日志、故障恢复、响应Gzip压缩中间件
+	echo_serve.Use(mws...)
+
+	echo_serve.Run(":8080")
 }
