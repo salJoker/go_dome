1b44c866cc1ce946f7250973510f1f37bd94d135
salJoker <huangchao387@sina.com>
2015-08-13 15:47:38 +0800

    Upload File Dome

--- src/main/main.go
+++ src/main/main.go
@@ -5,12 +5,13 @@ import (
 	mw "github.com/labstack/echo/middleware"
 	"user"
 	"webchat"
+	"upload"
 )
 
 func main(){
 	echo_serve := echo.New()
 
-	mws := []echo.Middleware{mw.Logger(),mw.Recover(),mw.Gzip()}
+	mws := []echo.Middleware{mw.Logger(),mw.Recover()}
 	//注册日志、故障恢复、响应Gzip压缩中间件
 	echo_serve.Use(mws...)
 
@@ -21,6 +22,7 @@ func main(){
 	echo_serve.Static("/","templates")
 
 	echo_serve.Post("/register",user.Register)
+	echo_serve.Post("/upload",upload.UploadFile)
 
 	echo_serve.WebSocket("/ws",webchat.WebChat)
 
--- src/upload/upload.go
+++ src/upload/upload.go
@@ -1,1 +1,35 @@
 package upload
+
+import(
+	"log"
+	"net/http"
+	"os"
+	"io"
+	"github.com/labstack/echo"
+)
+
+func UploadFile(c *echo.Context)error{
+	req := c.Request()
+	req.ParseMultipartForm(16 << 20)
+	files := req.MultipartForm.File["files"]
+	for _,file:= range files{
+		src,err := file.Open()
+		if err != nil{
+			return err
+		}
+		defer src.Close()
+
+		log.Println("FileName:",file.Filename)
+
+		dst,err := os.Create(file.Filename)
+		if err != nil{
+			return err
+		}
+		defer dst.Close()
+
+		if _,err := io.Copy(dst,src);err != nil {
+			return err
+		}
+	}
+	return c.String(http.StatusOK,"Upload Success!")
+}
