c531f85fbec763bddb8c09655858d630b451bf7b
salJoker <huangchao387@sina.com>
2015-08-14 17:57:50 +0800

    reflect type dome

--- src/main/main.go
+++ src/main/main.go
@@ -11,7 +11,7 @@ import (
 func main(){
 	echo_serve := echo.New()
 
-	mws := []echo.Middleware{mw.Logger(),mw.Recover()}
+	mws := []echo.Middleware{mw.Logger(),mw.Recover(),mw.Gzip()}
 	//注册日志、故障恢复、响应Gzip压缩中间件
 	echo_serve.Use(mws...)
 
--- src/main/reflectType.go
+++ src/main/reflectType.go
@@ -1,1 +1,70 @@
 package main
+
+import(
+	"fmt"
+	"reflect"
+)
+
+type Human interface {
+	SayName()string
+	Run()
+}
+
+type Man struct {
+	Name string
+}
+
+func NewMan()*Man{
+	return &Man{Name:"tomc"}
+}
+
+func (m *Man) SayName()string{
+	return m.Name
+}
+
+func (m *Man) Run(){
+	fmt.Println("%s is runing",m.Name)
+}
+
+type Woman struct{
+	Name string
+}
+
+func NewWoman()*Woman{
+	return &Woman{Name:"ali"}
+}
+
+func (w *Woman) SayName()string{
+	return w.Name
+}
+
+func (w *Woman) Run(){
+	fmt.Println("%s is runing",w.Name)
+}
+
+func main(){
+	m := NewMan()
+
+	mType := reflect.TypeOf(m)
+	fmt.Println("name:",mType.Name())
+	fmt.Println("Kind:",mType.Kind())
+	fmt.Println("string:",mType.String())
+	fmt.Println("method num:",mType.NumMethod())
+
+	if method,exists := mType.MethodByName("Run");exists{
+		fmt.Println("method.Name:",method.Name)
+		fmt.Println("method.Value:",method.Func)
+		fmt.Println("method.Type.NumIn:",method.Type.NumIn())
+
+	}else{
+		fmt.Println("method not exists")
+	}
+
+	mo := reflect.PtrTo(mType)
+	fmt.Println("ptr:",mo)
+
+	m_val := reflect.ValueOf(m)
+	fmt.Println("value:",m_val)
+
+	fmt.Println(reflect.Indirect(m_val))
+}
