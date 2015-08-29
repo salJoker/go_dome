0464ce892475f434138551b2ab502436d4f1dcb6
salJoker <huangchao387@sina.com>
2015-08-29 18:20:28 +0800

    TempDir

--- src/main/driver.go
+++ src/main/driver.go
@@ -1,32 +1,55 @@
 package main
 
 import(
-	"os"
+	//"os"
 	"fmt"
-	"path/filepath"
+	//"path/filepath"
 	"io/ioutil"
+	"os"
 )
 
 func main(){
 //	for i,val := range os.Args {
 //		fmt.Printf("%s : %d",val,i)
 //	}
-	driver := os.Args[1]
-	drv,err := filepath.Abs(driver)
+
+//	driver := os.Args[1]
+//	drv,err := filepath.Abs(driver)
+//	if err != nil {
+//		return
+//	}
+//	fileindos,err := ioutil.ReadDir(drv)
+//	if err != nil {
+//		return
+//	}
+//	fmt.Printf("fileinfos length : %d\n",len(fileindos))
+//	var fileCount int = 0
+//	for _,fileinfo := range fileindos {
+//		if !fileinfo.IsDir(){
+//			fmt.Printf("filename:%s\n",fileinfo.Name())
+//			fileCount++
+//		}
+//	}
+//	fmt.Println("file count : ",fileCount)
+
+	//name,err := ioutil.TempDir("","go_tmp")//使用系统默认临时目录，目录名称：go_temp+随机数
+	name,err := ioutil.TempDir("resources","go_temp")//使用相对路径，在当前GOPATH目录下的resources中创建临时目录
 	if err != nil {
+		fmt.Println(err.Error())
 		return
 	}
-	fileindos,err := ioutil.ReadDir(drv)
-	if err != nil {
+	fmt.Println(name)
+
+	//创建临时文件，与ioutil.TempDir类似
+	//filename,err := ioutil.TempFile("","go_temp")//使用系统默认临时目录
+	filename,err := ioutil.TempFile(name,"go_temp")
+	if err != nil{
 		return
 	}
-	fmt.Printf("fileinfos length : %d\n",len(fileindos))
-	var fileCount int = 0
-	for _,fileinfo := range fileindos {
-		if !fileinfo.IsDir(){
-			fmt.Printf("filename:%s\n",fileinfo.Name())
-			fileCount++
-		}
-	}
-	fmt.Println("file count : ",fileCount)
+	fmt.Println(filename.Name())
+
+	defer func(){
+		filename.Close()
+		os.Remove(filename.Name())
+	}()
 }
